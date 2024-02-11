ff99fb74827c7b1a4207d375e5746383f6d2b7fa
Daniil Kovalenko <danielkovalenko50@gmail.com>
2024-02-11 18:01:52 +0300

    cleanup

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
 export const trim = (str, length, words = 10) => {
-    console.log("got:", str);
     const wordsArray = str.split(" ")
     if (length && str.length > length) {
         return str.substring(0, length - 3) + "...";
